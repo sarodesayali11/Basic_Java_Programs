58b2e50329fb8f47a7a08156c5f06a8cf4a15b7d
Sayali A.Sarode <sarodesayali11@gmail.com>
2024-05-28 13:31:14 +0530

    Add files via upload

---
+++ Day.java
@@ -0,0 +1,36 @@
+public class Day
+{
+public static void main(String args[])
+{
+int no = Integer.parseInt(args[0]);
+if(no==1)
+{
+  System.out.println("Monday");
+}
+else if(no==2)
+{
+  System.out.println("Tuesday");
+}
+else if(no==3)
+{
+  System.out.println("Wednesday");
+}
+else if(no==4)
+{
+  System.out.println("Thursday");
+}
+else if(no==5)
+{
+  System.out.println("Friday");
+}
+else if(no==6)
+{
+  System.out.println("Saturday");
+}
+else if(no==7)
+{
+  System.out.println("Sunday");
+}
+
+}
+}
