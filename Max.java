87b93c5c088306c2e42d4b7918361624d9f99ed1
Sayali A.Sarode <sarodesayali11@gmail.com>
2024-05-28 13:35:41 +0530

    Add files via upload

---
+++ Max.java
@@ -0,0 +1,13 @@
+public class Max
+{
+public static void main(String args[])
+{
+int a = Integer.parseInt(args[0]);
+int b = Integer.parseInt(args[1]);
+if(a>b)
+System.out.println(" a is max"  );
+else
+System.out.println("b is greater" );
+
+}
+}
