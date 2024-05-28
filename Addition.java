ee811a476143354ec005720732c577f5228ed6b5
Sayali A.Sarode <sarodesayali11@gmail.com>
2024-05-28 13:28:17 +0530

    Add files via upload
    Addition of Two Numbers

---
+++ Addition.java
@@ -0,0 +1,19 @@
+public class Addition
+{
+public static void main(String args[])
+{
+   System.out.println("Addition of Two Number");
+   int num = Integer.parseInt(args[0]);
+   int num1 = Integer.parseInt(args[1]);
+   int sum = num+num1;
+   System.out.println("The sum is " + sum);
+}
+}
+
+public class Simple
+{
+public static void main(String args[])
+{
+       System.out.println("First Java Program");
+}
+}
