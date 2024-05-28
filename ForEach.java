74760618c32230d4f20347285cd0cb4afd9fe60d
Sayali A.Sarode <sarodesayali11@gmail.com>
2024-05-28 13:32:59 +0530

    Add files via upload

---
+++ ForEach.java
@@ -0,0 +1,10 @@
+public class ForEach {
+       public static void main(String[] args)
+{
+       int[] numbers= {1,2,3,4,5};
+
+//Using for-each loop to iterate over elements of the array
+       for(int number : numbers)
+       System.out.println(number);
+}
+}
