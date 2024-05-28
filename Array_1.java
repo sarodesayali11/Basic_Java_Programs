76000471452870ae79f8b7807b96e5639e5386d3
Sayali A.Sarode <sarodesayali11@gmail.com>
2024-05-28 13:29:21 +0530

    Add files via upload

---
+++ Array_1.java
@@ -0,0 +1,25 @@
+public class Array_1
+{
+public static void main(String[] args)
+{
+int a[] = new int[5];
+a[0]=1;
+a[1]=2;
+a[2]=3;
+a[3]=4;
+a[4]=5;
+System.out.println(a[0]);
+System.out.println(a[1]);
+System.out.println(a[2]);
+System.out.println(a[3]);
+System.out.println(a[4]);
+
+System.out.println("Output using Foreach loop");
+    for(int x:a)
+    {
+    System.out.println(x);
+    }
+
+
+
+ }}
