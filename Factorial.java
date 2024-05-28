178fbbb707655da0a70ced1f28a2f4b7c1cfa29f
Sayali A.Sarode <sarodesayali11@gmail.com>
2024-05-28 13:32:06 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+public class Fact
+{
+public static void main(String args[])
+{
+int fact=1;
+int no=5;
+for(int i=1;i<=no;i++)
+{
+     fact = fact*i;
+}
+System.out.println("Factotrial of given no. is " +fact);
+}
+}
